47560ab4f1b7a343fd1642331e4680bad5a57d3b
Thiago <thiagosbuarque@gmail.com>
2022-06-02 23:32:37 -0300

    Criar lista de palavras inicial e escolher uma palavra aleatória

---
+++ inicio.py
@@ -0,0 +1,7 @@
+lista_de_palavras = ["ardvark", "baboon", "camel"]
+
+# Escolher uma palavra aleatória da lista_de_palavras e atribuí-la a uma variável chamada 'palavra_escolhida'
+
+import random
+
+chosen_word = random.choice(lista_de_palavras)
